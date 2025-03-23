8790b003e6df52f8cb9bf94b53362dd1f83992b0
mj-una <133172570+mj-una@users.noreply.github.com>
2025-03-23 09:22:52 -0300

    publicar en gh pages. intento 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	output: 'static',
+  site: 'https://mj-una.github.io',
+  base: 'blog-whatsapp-prueba1',
+	trailingSlash: "always"
+})
